c8ed3ee8d4d0964324e0c3f76bf25daec7be9d2c
Aloysius Elvan <aloysiuselvan98@gmail.com>
2026-03-08 21:09:33 +0700

    fix gateway to production

--- gateway/next.config.ts
+++ gateway/next.config.ts
@@ -3,18 +3,21 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
-      // Ganti URL localhost dengan URL produksi dari dashboard Vercel Anda
-      { source: '/v1', destination: 'https://nomadic-voice-v1.vercel.app/' },
-      { source: '/v1/:path*', destination: 'https://nomadic-voice-v1.vercel.app/' },
+      // Versi 1
+      { source: '/v1', destination: 'https://nomadic-voice-v1.vercel.app/v1' },
+      { source: '/v1/:path*', destination: 'https://nomadic-voice-v1.vercel.app/v1/:path*' },
 
-      { source: '/v2', destination: 'https://nomadic-voice-v2.vercel.app/' },
-      { source: '/v2/:path*', destination: 'https://nomadic-voice-v2.vercel.app/' },
+      // Versi 2
+      { source: '/v2', destination: 'https://nomadic-voice-v2.vercel.app/v2' },
+      { source: '/v2/:path*', destination: 'https://nomadic-voice-v2.vercel.app/v2/:path*' },
 
-      { source: '/v3', destination: 'https://nomadic-voice-v3.vercel.app/' },
-      { source: '/v3/:path*', destination: 'https://nomadic-voice-v3.vercel.app/' },
+      // Versi 3
+      { source: '/v3', destination: 'https://nomadic-voice-v3.vercel.app/v3' },
+      { source: '/v3/:path*', destination: 'https://nomadic-voice-v3.vercel.app/v3/:path*' },
 
-      { source: '/v4', destination: 'https://nomadic-voice-v4.vercel.app/' },
-      { source: '/v4/:path*', destination: 'https://nomadic-voice-v4.vercel.app/' },
+      // Versi 4
+      { source: '/v4', destination: 'https://nomadic-voice-v4.vercel.app/v4' },
+      { source: '/v4/:path*', destination: 'https://nomadic-voice-v4.vercel.app/v4/:path*' },
     ];
   },
 };
